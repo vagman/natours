70b76634d129505059056f904978e3b7da0f976d
vagman <bmanousakis@gmail.com>
2025-11-11 12:40:56 +0200

    fixed typo

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ import tourRouter from './routes/tourRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import reviewRouter from './routes/reviewRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
-import bookingController from './routes/bookingController.js';
+import bookingController from './controllers/bookingController.js';
 import viewRouter from './routes/viewRoutes.js';
 
 const app = express();
